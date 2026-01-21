80ad1603d49f884a089ca6e300a0b8ac8d710601
Simaoui Imed <simaouiimed@MacBook-Pro-de-Simaoui.local>
2026-01-21 23:38:04 +0100

    Set homepage route and design

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
+    #[Route('/home', name: 'app_home_legacy')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
